8b4c864eeb9895e35418e1fd0d39dcbf24c1b2a8
Maxim Liutyi <maks.zloy@ukr.net>
2026-02-20 23:17:13 +0100

    aws_resource_tracer

---
+++ aws_project_using_sell/aws_resource_tracker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+###############
+# Author: Maksym
+# Date: 20.2.206
+# This scrip will show aws resourses usage
+# You also need to configure AWS CLI
+##############
+
+# AWS S3
+# AWS EC2
+# AWS lambda
+# AWS IAM Users
+
+#set -x # debug
+
+echo "Print list of s3 buckets:"
+# List S3 buckets
+aws s3 ls
+
+echo "Print list of ec2:"
+# List EC2 Instances
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+echo "Print lambda functions:"
+# List AWS lambda
+aws lambda list-functions
+
+echo "Print IAM users:"
+# List IAM users
+aws iam list-users
+
+
